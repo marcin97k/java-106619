6beac985ba144ae67db0b858ed1e410b475b9f8e
106619 <106619@U-55164.domena.uksw.edu.pl>
2019-10-26 14:01:08 +0200

    Trójkąt

--- Triangle/src/trojkat.java
+++ Triangle/src/trojkat.java
@@ -7,16 +7,22 @@ public class trojkat {
 	{
 		if((a+b>c)&&(a+c>b)&&(b+c>a))
 		{
+			System.out.print("Mo¿na z³o¿yæ trójk¹t: ");
 			double p=((a+b+c)/2);
 			return (double)Math.sqrt(p*(p-a)*(p-b)*(p-c));
-		}else return 0;
+		} else {
+			System.out.print("Nie mo¿na z³o¿yæ trójk¹ta.");
+			System.out.println();
+			return 0;
+			}
 	}
-	  	
-	public static void main(String[] args) {
+		  	
+	public static void main(String[] args)
+	{
 		int a=Integer.parseInt(JOptionPane.showInputDialog("Podaj a: "));
 		int b=Integer.parseInt(JOptionPane.showInputDialog("Podaj b: "));
 		int c=Integer.parseInt(JOptionPane.showInputDialog("Podaj c: "));
 		System.out.print("Pole trójk¹ta: "+poletr(a,b,c));
+	}
 	
-	}
-	}
+}
