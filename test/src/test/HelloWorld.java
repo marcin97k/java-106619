e27ef8eed9e2db18fb85960351d7de37203d35ad
106619 <106619@U-55164.domena.uksw.edu.pl>
2019-10-26 12:27:42 +0200

    TEST

---
+++ test/src/test/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	
+	public static void main(String[] args ) {
+		
+		System.out.print("Hello World!");
+		
+		String hello = "Hello world!";
+		
+		System.out.print("Napis 'Hello World' ma " + hello.length() + " znakow");
+		
+		
+	}
+}
