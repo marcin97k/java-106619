d8f4561ec08c6f51bc619dd86f1ac8097b40c8cc
106619 <106619@U-55164.domena.uksw.edu.pl>
2019-12-07 15:30:09 +0100

    zadanie 5

---
+++ Main/zajecia 7.12/People.java
@@ -0,0 +1,23 @@
+public class People extendts People2 {
+
+	Set<Person> people = new HashSet<>();
+	people.add(new Person("Jan", "Kowalski", 25));
+	people.add(new Person("Jan", "Polak", 31));
+	people.add(new Person("Teodor", "Kowalski", 80));
+	people.add(new Person("Jan", "Kliczko", 45));
+	people.add(new Person("Bart", "Turek", 75));
+	people.add(new Person("Jan", "Kowalski", 25));
+	 
+	
+	public static void main(String[] args) {
+
+		Set<String> newset = new TreeSet<>(); 
+		public int compareTo(People o) {
+			return this.age.compareTo(o.age));
+		}
+		 
+		
+	}
+
+	
+}
