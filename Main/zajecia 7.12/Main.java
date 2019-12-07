5d22f0e582e21440ccfcbd6d8cb050d4b0315201
106619 <106619@U-55164.domena.uksw.edu.pl>
2019-12-07 15:59:28 +0100

    zadanie 1

---
+++ Main/zajecia 7.12/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+	
+	void contains<T> {
+	ArrayList<String> listaString = new ArrayList<String>();
+	private T item;
+	}
+
+
+  public static void main(String[] args) {
+
+    String[] array = {"element", "item"};
+
+    String element1 = "element";
+
+    String element2 = "element2";
+
+    for (int i = 0; i<=array.length; i++) {
+    	if (item==array(i)) int temp = 1; break;
+    	else int temp = 0;
+    }
+    return temp;
+    
+
+    Main main = new Main();
+
+    System.out.println(main.contains(array, element1));
+
+    System.out.println(main.contains(array, element2));
+
+    
+
+    
+
+  }
+
+}
